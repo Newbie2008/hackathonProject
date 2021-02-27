0ab2422ed79185acbd5b87ccefae06b3a0c814c6
Icode-python <me.fast.learn@gmail.com>
2021-02-26 19:17:34 -0500

    Co-authored-by: Veerrohit Veeravadivel <Newbie2008@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+import sys, random
+from settings import *
+
+class Game:
+    def __init__(self):
+        
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(TITLE)
+        self.clock = pygame.time.Clock()
