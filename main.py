75301da82bf2e1b21df2287c05af5e6bc00a8d50
Icode-python <me.fast.learn@gmail.com>
2021-02-26 19:24:36 -0500

    created main.py

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from settings import *
 
 class Game:
     def __init__(self):
-        
+
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption(TITLE)
         self.clock = pygame.time.Clock()
+
+        
