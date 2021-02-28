cb5e1c8c601d6d6f1838a859f7bfaf9502463401
Newbie2008 <veerrohitv08@gmail.com>
2021-02-27 20:23:51 -0800

    edited mapmakingscripts

--- scripts/map-making-scripts.py
+++ scripts/map-making-scripts.py
@@ -1,8 +1,11 @@
 import random
 import sys
 
-rows = input("Enter the number of rows")
-columns = input("Enter the number of columns")
+rows = input("Enter the number of rows: ")
+columns = input("Enter the number of columns: ")
+
+rows = int(rows)
+columns = int(columns)
 
 x = 0
 map_arr = ['.', '1']
@@ -11,14 +14,14 @@ while i in range(0,rows):
     if i == 0:
         for k in range(0, columns):
             sys.stdout.write("1")
-    elif i == 39:
+    elif i == columns-1:
         for l in range(0, columns):
             sys.stdout.write("1")
     else:
         for j in range(0, columns):
             if j == 0:
                 sys.stdout.write("1")
-            elif j == 39:
+            elif j == columns-1:
                 sys.stdout.write("1")
             else:
                 x = random.randint(0,1)
