76bb30ad8fdc2b828e88664f6e61a21c8f7a6eb8
Newbie2008 <veerrohitv08@gmail.com>
2021-02-27 20:01:53 -0800

    made the map making script better

--- scripts/map-making-scripts.py
+++ scripts/map-making-scripts.py
@@ -6,7 +6,12 @@ map_arr = ['.', '1']
 i = 0
 while i in range(0,40):
     for j in range(0, 40):
-        x = random.randint(0,1)
-        sys.stdout.write(map_arr[x])
+        if j == 0:
+            sys.stdout.write("1")
+        elif j == 39:
+            sys.stdout.write("1")
+        else:
+            x = random.randint(0,1)
+            sys.stdout.write(map_arr[x])
     i = i+1
     print("")
